400911d19d78fbdd74776d89a657bb9400726a10
Asmaa Sadou <as1304.ammar@gmail.com>
2023-05-25 13:08:35 +0200

    add info

--- hb-test/src/App.js
+++ hb-test/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>INTERNATIONAL NGO</h1>
+      <Main />
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ hb-test/src/components/Header.js
@@ -0,0 +1,12 @@
+export default function header() {
+  return (
+    <header>
+      <h1>START X CHANGE</h1>
+      <p>
+        Welcome to our platform, bridging NGOs globally for shared projects,
+        ideas, and meaningful dialogue. Join us in driving positive change and
+        amplifying social impact through collaboration and collective action
+      </p>
+    </header>
+  );
+}
